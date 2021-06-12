5719145829eec7beeb2c931e768fa256ba745eda
Artem <TimF1234@yandex.ru>
2021-06-12 13:50:26 +0300

    customizing styles

--- form.js
+++ form.js
@@ -1,21 +1,30 @@
 let json = {
     basicComp: [
+        {tag: "h1", id: "heading", title: "Анкета на найм к Торину и КО"},
         {tag: "input", type: "text", id: "first_name", title: "Имя"}, 
         {tag: "input", type: "text", id: "last_name", title: "Фамилия"},
         {tag: "input", type: "tel", id: "phone", title: "Телефон(ворон)"},
         {tag: "input", type: "email", id: "email", title: "Почта"},
         {tag: "input", type: "number", id: "age", title: "Возраст"},
-        {tag: "input", type: "date", id: "date", title: "", value:"2021-06-10"},
-        {tag: "input", type: "radio", id: "rings", title: ["Есть", "Нет"], name: "rings", value: ["Yes", "No"]},
+        {tag: "input", type: "date", id: "date", title: "Дата:", value:"2021-06-10"},
+        {tag: "p", id: "paragraph", title: "Волшебные кольца:"},
+        {tag: "input", id: "rings", type: "radio", title: ["Есть", "Нет"], name: "rings", value: ["Yes", "No"]},
         {tag: "p", id: "paragraph", title: "Специализации:"},
         {tag: "input", type: "checkbox", id: "specialization", title: ["Взломщик", "Воин", "Стрелок", "Маг"], name: "specialization", value: ""},
     ],
 
     additionalComp: [
-        {tag: "select", id: "listRings", optionValue: ["value1", "value2", "value3", "value4"], optionTitle: ["Кольцо Всевластья", "Кольцо людей", "Кольцо эльфов", "Кольцо гномов"]}, 
+        {tag: "select", id: "listRings", title: "Список колец:", optionValue: ["value1", "value2", "value3", "value4"], optionTitle: ["Кольцо Всевластья", "Кольцо людей", "Кольцо эльфов", "Кольцо гномов"]},
+        {tag: "input", id: "submit", type: "submit", value:"Отправить"},
     ],
 
-    preferencesInfo: [],
+    preferencesComp: [],
+
+    renderOrder: [
+        "basicComp",
+        "additionalComp",
+        "preferencesComp"
+    ]
 }
 
 export default json;
--- import-json.js
+++ import-json.js
@@ -1,39 +1,42 @@
 import json from './form.js';
 
 /**
- * 
- * @param {Object[]} json 
+ *
+ * @param {Object[]} json
  * @returns {Object[]}
  */
 function getComponents(json) {
-    let arr = []
-    for(let key in json) {
+    let arr = [];
+    for (let key of json['renderOrder']) {
         arr.push(json[key]);
     }
     return arr;
 }
 
 /**
- * 
- * @param {Object} CompArr 
+ *
+ * @param {Object} json
  * @returns {String}
  */
-function generateForm(CompArr) {
-    let arrComp = getComponents(CompArr);
-    let component = generateComponent(arrComp); 
-    let formStr = `<form action="#" method="post" enctype="multipart/form-data">\n\t${component}</form>`;
-
-    return formStr;
+function generateForm(json) {
+    const arrComp = getComponents(json);
+    let formStr = '';
+    for (const component of arrComp) {
+        formStr += '\t';
+        formStr += generateComponent(component);
+        formStr += '\n';
+    }
+    return `<form class="form" action="#" method="post" enctype="multipart/form-data">\n\t${formStr}\n${style()}\n</form>`;
 }
 
 /**
- * @param {Object[]} arrComp
+ * @param {Object[]} component
  * @returns {String}
  */
-function generateComponent(arrComp) {
+function generateComponent(component) {
     let compStr = '';
-    for (let i = 0; i < arrComp.length; i++) {
-        compStr += generateTag(arrComp[i]);
+    for (const tag of component) {
+        compStr += generateTag(tag);
         compStr += '\n';
         compStr += '\t';
     }
@@ -41,22 +44,11 @@ function generateComponent(arrComp) {
 }
 
 /**
- * 
- * @param {Object} objectTag
+ *
+ * @param {Object} obj
  * @returns {String}
  */
-function generateTag(objectTag) {
-    let strTag = '';
-    for (let i = 0; i < objectTag.length; i++) {
-        strTag += inputs(objectTag[i]);
-        strTag += '\n';
-        strTag += '\t';
-    }
-    return strTag;
-}
-
-
-function inputs(obj) {
+function generateTag(obj) {
     if (obj.type === 'text' || obj.type === 'number') {
         return input(obj);
     } else if (obj.type === 'tel') {
@@ -73,55 +65,154 @@ function inputs(obj) {
         return checkbox(obj);
     } else if (obj.tag === 'p') {
         return paragraph(obj);
+    } else if (obj.tag === 'h1') {
+        return heading1(obj);
+    } else if (obj.type === 'submit') {
+        return submit(obj);
     }
+
 }
 
 
 function input(obj) {
-    let text = `\n\t<label for="${obj.id}">${obj.title}</label>\n\t<input id="${obj.id}" type="${obj.type}"/>`;
-    return text;
+    return `<div class="indent">\n\t\t<label for="${obj.id}">${obj.title}</label>\n\t\t<input id="${obj.id}" type="${obj.type}"/>\n\t</div>`;
 }
+
 function inputPhone(obj) {
-    let phone = `\n\t<label for="${obj.id}">${obj.title}</label>\n\t<input id="${obj.id}" type="${obj.type}"/>`;
-    return phone;
+    return `<div class="indent">\n\t\t<label for="${obj.id}">${obj.title}</label>\n\t\t<input id="${obj.id}" type="${obj.type}"/>\n\t</div>`;
 }
+
 function inputEmail(obj) {
-    let email = `\n\t<label for="${obj.id}">${obj.title}</label>\n\t<input id="${obj.id}" type="${obj.type}"/>`;
-    return email;
+    return `<div class="indent">\n\t\t<label for="${obj.id}">${obj.title}</label>\n\t\t<input id="${obj.id}" type="${obj.type}"/>\n\t</div>`;
 }
+
 function inputDate(obj) {
-    let date = `\n\t<label for="${obj.id}">${obj.title}</label>\n\t<input id="${obj.id}" type="${obj.type}" value="${obj.value}"/>`; 
-    return date;
+    return `<div class="indent">\n\t\t<label for="${obj.id}">${obj.title}</label>\n\t\t<input id="${obj.id}" type="${obj.type}" value="${obj.value}"/>\n\t</div>`;
 }
+
+function submit(obj) {
+    return `<input type="${obj.type}" value="${obj.value}">`
+}
+
 function inputRadio(obj) {
     let strRadios = '';
     for (let i = 0; i < obj.title.length; i++) {
-        let radio = `\n\t<label for="${obj.id}">${obj.title[i]}</label>\n\t<input id="${obj.id}" type="${obj.type}" value="${obj.value[i]}" name="${obj.name}">`;
+        let radio = `\n\t<div>\n\t\t<input id="${obj.id}" type="${obj.type}" value="${obj.value[i]}" name="${obj.name}">\n\t\t<label for="${obj.id}">${obj.title[i]}</label>\n\t</div>`;
         strRadios += radio;
     }
     return strRadios;
 }
+
 function select(obj) {
     let strOptions = '';
     for (let i = 0; i < obj.optionTitle.length; i++) {
         let option = `\n\t\t<option value="${obj.optionValue[i]}">${obj.optionTitle[i]}</option>`;
         strOptions += option;
     }
-    let selectTag = `<select id="${obj.id}">${strOptions}\n\t</select>`;
+    let selectTag = `<div>\n\t\t<label for="${obj.id}">${obj.title}</label>\n\t\t<select id="${obj.id}">${strOptions}\n\t</select>\n\t</div>`;
     return selectTag;
 }
+
 function checkbox(obj) {
     let strCheckbox = '';
     for (let i = 0; i < obj.title.length; i++) {
-        let checkbox = `\n\t<label for="${obj.id}">${obj.title[i]}</label>\n\t<input type="${obj.type}" id="${obj.id}" name="${obj.name}">`;
+        let checkbox = `\n\t<div>\n\t\t<input type="${obj.type}" id="${obj.id}" name="${obj.name}">\n\t\t<label for="${obj.id}">${obj.title[i]}</label>\n\t</div>`;
         strCheckbox += checkbox;
     }
     return strCheckbox;
 }
 
 function paragraph(obj) {
-    let tagParagraph = `\n\t<p>${obj.title}</p>`;
-    return tagParagraph;
+    return `\n\t<p ${obj.id}>${obj.title}</p>`;
 }
 
+function heading1(obj) {
+    return `<h1 ${obj.id}>${obj.title}</h1>`;
+}
+
+
+// style CSS
+function style() {
+    return (`
+    <style>
+        /* Remove default margin */
+        body,
+        h1,
+        h2,
+        h3,
+        h4,
+        p {
+            margin: 0;
+        }
+        
+
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        .form {
+            display: flex;
+            flex-direction: column;
+            width: 400px;
+            padding: 25px 32px;
+            font-family: sans-serif;
+            box-shadow: 0 2px 28px rgb(0 0 0 / 21%);
+            letter-spacing: 0.5px;
+        }
+
+        .form h1 {
+            text-align: center;
+            padding: 0px 0 15px 0;
+        }
+
+        .form p {
+            padding: 15px 0 3px 0;
+        }
+
+        .form input {
+            outline:none;
+            padding: 5px;
+            margin: 5px 0 5px 0;
+            border: 0.5px solid rgb(216, 216, 216); 
+        }
+        .form input:hover {
+            border: 0.5px solid rgb(185, 185, 185); 
+        }
+        .form input:focus {
+            border-color: gold; 
+        }
+
+        .form select {
+            outline: none;
+            width: auto;
+            padding: 5px;
+            margin: 10px 0 10px 0;
+        }
+
+        .form label {
+            margin: 0 10px 0 0;
+        }
+
+        .form .indent {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        .form .submit {
+            width: 30%;
+            margin: 15px auto;
+            padding: 10px 10px;
+            background-color: ghostwhite;
+        }
+        .form .submit:hover {
+            cursor: pointer;
+            border-color: gold; 
+        }
+    </style>`
+    )
+}
 console.log(generateForm(json));
